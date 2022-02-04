c3e863bddee02badb0b5f9a63556ffde31aefe6b
Mykhailo <m_musienko@outlook.com>
2022-02-04 13:53:51 +0200

    Update Unit.java

--- src/com/mykhailo/Unit.java
+++ src/com/mykhailo/Unit.java
@@ -1,4 +1,33 @@
 package com.mykhailo;
 
-public class Unit {
+public abstract class Unit {
+
+    private Name name;
+    private boolean isImmortal;
+    private boolean canMove;
+    private boolean canFire;
+    private boolean canReload;
+
+    protected Unit(Name name, boolean isImmortal, boolean canMove, boolean canFire, boolean canReload) {
+        this.name = name;
+        this.isImmortal = isImmortal;
+        this.canMove = canMove;
+        this.canFire = canFire;
+        this.canReload = canReload;
+    }
+
+    public Name getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Unit{" +
+                "name=" + name +
+                ", isImmortal=" + isImmortal +
+                ", canMove=" + canMove +
+                ", canFire=" + canFire +
+                ", canReload=" + canReload +
+                '}';
+    }
 }
